b35b1c0aa472ec416deaae000c14b657e0383ea3
DmitriySokolov <frakinf999@gmail.com>
2024-01-19 13:54:33 +0500

    tweaked the code

--- Lesson4/Homework.Lesson4.Task4/Program.cs
+++ Lesson4/Homework.Lesson4.Task4/Program.cs
@@ -4,26 +4,26 @@ class Program
 {
     static void Main()
     {
-        // Вводим размер массива с консоли
+        // Ввод размера массива с консоли
         Console.Write("Введите размер массива (не более 8): ");
         int N = Convert.ToInt32(Console.ReadLine());
 
-        // Проверяю на допустимость размера массива
+        // Проверка на допустимость размера массива
         if (N <= 0 || N > 8)
         {
-            Console.WriteLine("Неверно задан размер массива. Размер должен быть от 1 до 8.");
+            Console.WriteLine("Недопустимый размер массива. Размер должен быть от 1 до 8.");
             return;
         }
 
-        // Созданю массив и заполняю случайными числами от 0 до 9
+        // Создание массива и заполнение случайными числами от 0 до 9
         int[] array = new int[N];
-        Random randomDigit = new Random();
+        Random random = new Random();
         for (int i = 0; i < N; i++)
         {
-            array[i] = randomDigit.Next(10);
+            array[i] = random.Next(10);
         }
 
-        // Вывожу массив на экран
+        // Вывод массива
         Console.Write("Массив: ");
         for (int i = 0; i < N; i++)
         {
@@ -31,7 +31,11 @@ class Program
         }
         Console.WriteLine();
 
-        // Формируем целое число из цифр массива с нужным порядком
+        // Сортировка массива в порядке убывания
+        Array.Sort(array);
+        Array.Reverse(array);
+
+        // Сформировать целое число из отсортированных цифр массива
         int result = 0;
         for (int i = 0; i < N; i++)
         {
--- Lesson4/Homework.Lesson4.Task4/obj/Debug/net8.0/Homework.Lesson4.Task4.AssemblyInfo.cs
+++ Lesson4/Homework.Lesson4.Task4/obj/Debug/net8.0/Homework.Lesson4.Task4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Homework.Lesson4.Task4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f0adaa1eacc2d1e0af3e8e53a7d966df6c90ab6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b61a5d0f513cb599b8ce086063b1783b947efb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Homework.Lesson4.Task4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Homework.Lesson4.Task4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
