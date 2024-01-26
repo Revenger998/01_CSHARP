5f4ef23e359a08b5551faf3986b0296efbcfd917
DmitriySokolov <frakinf999@gmail.com>
2024-01-26 13:19:32 +0500

    corrected code

--- Lesson6/Homework.Task2/Program.cs
+++ Lesson6/Homework.Task2/Program.cs
@@ -3,9 +3,10 @@
 class Program
 {
     static void Main(string[] args)
-{   // Создаем строку содержащую буквы разного регистра
-    string stringOfLetters = "LaNgUaGe";
-    string result = stringOfLetters.ToLower();
-    Console.WriteLine(result);
-}
+    {   // Создаем строку содержащую буквы разного регистра
+        string stringOfLetters = "LaNgUaGe";
+        // Изменяем регистр букв в строке
+        string result = stringOfLetters.ToLower();
+        Console.WriteLine(result);
+    }
 }
