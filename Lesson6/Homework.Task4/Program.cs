28661d0654988a8e7634a1a2f891ce2cc284223f
DmitriySokolov <frakinf999@gmail.com>
2024-01-26 13:40:00 +0500

    added the solution to the fourth task

---
+++ Lesson6/Homework.Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    // Используем метод ReverseWords с помощью которого разворачиваем строку, меняя порядок слов на обратный
+    static string ReverseWords(string str)
+    {
+        // Получаем слова из строки, разделенные пробелами
+        string[] words = str.Split(' ');
+        // Разворачиваем порядок слов
+        Array.Reverse(words);
+        // Объединяем слова в строку, разделяя их пробелами
+        return string.Join(" ", words);
+    }
+
+    static void Main(string[] args)
+    {   // Создаем строку со словами разделенными пробелами
+        string strOfWordsBySpaces = "String of words by spaces";
+        // Вызываем метод ReverseWords для разворота строки
+        string finalString = ReverseWords(strOfWordsBySpaces);
+        Console.WriteLine(finalString);
+    }
+}
