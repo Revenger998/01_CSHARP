ad8225ef53c5d6bb0e186e3efed9a394cb8a9587
DmitriySokolov <frakinf999@gmail.com>
2024-01-26 13:18:42 +0500

    added the solution to the second task

---
+++ Lesson6/Homework.Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+{   // Создаем строку содержащую буквы разного регистра
+    string stringOfLetters = "LaNgUaGe";
+    string result = stringOfLetters.ToLower();
+    Console.WriteLine(result);
+}
+}
