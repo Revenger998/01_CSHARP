f2bc27ad1f122030b0483c643e2db463a4b4e8e8
DmitriySokolov <frakinf999@gmail.com>
2024-01-26 13:03:19 +0500

    added the solution to the first task

--- Lesson6/Homework.Task1/Program.cs
+++ Lesson6/Homework.Task1/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Задаем двумерный массив символов 4х3  
+        char[,] rndArray = new char[4, 3]{
+        {'$','6','1'},
+        { '5','#','!'},
+        { 'k','%','&'},
+        { '8','*','@'}};
+        // Создаем пустую строку
+        string newString = " ";
+        // Заполняем строку символами из массива
+        for (int i = 0; i < rndArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < rndArray.GetLength(1); j++)
+            {
+                newString += rndArray[i, j].ToString();
+            }
+        }
+        Console.WriteLine(newString);
+    }
+}
